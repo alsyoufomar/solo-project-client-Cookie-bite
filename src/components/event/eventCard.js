520614c268f123dd059cca613fbee72be24580c8
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-05-22 12:17:00 +0100

    card styling

--- src/components/event/eventCard.js
+++ src/components/event/eventCard.js
@@ -1,30 +1,37 @@
+import './cardStyle.css';
+
 export default function EventCard(props) {
   const { card } = props;
   return (
     <li className='event'>
-      <div className='flip-card-inner'>
-        <div className='event--info'>
+      <a href={card.url} rel='noreferrer' target='_blank'>
+        <div className='event__image'>
           <span className='featured-tag'>Featured</span>
-          <div className='event__image'>
-            <img src={card.image} alt={card.title} />
-          </div>
-          <div className='event__date'>
-            <i className='fa-solid fa-calendar-day'></i>
-            {card.date}
-          </div>
-          <div className='event__title'>
-            <i className='fa-solid fa-location-dot'></i>
-            {card.bigTitle}
-          </div>
+          <img src={card.image} alt={card.title} />
         </div>
-        <a
-          className='event__btn'
-          href={card.url}
-          rel='noreferrer'
-          target='_blank'>
-          visit website
-        </a>
+      </a>
+      <div className='fav-list'>
+        <i class='fa-regular fa-bookmark'></i>
       </div>
+      <ul className='event__info'>
+        <li className='event__info-item event__title'>
+          <a href={card.url} rel='noreferrer' target='_blank'>
+            {card.title}
+          </a>
+        </li>
+        <li className='event__info-item event__date'>
+          <i className='fa-solid fa-calendar-day'></i>
+          {card.date ? card.date.trim() : card.date}
+        </li>
+        <li className='event__info-item event__time'>{card.time}</li>
+        <li className='event__info-item event__address'>
+          <i className='fa-solid fa-location-dot'></i>
+          {card.fullAddress
+            ? card.fullAddress.replace(/\+/gm, '\n')
+            : card.location}
+        </li>
+        <li className='event__info-item event__ageLimit'>{card.age}</li>
+      </ul>
     </li>
   );
 }
